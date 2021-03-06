225bb8d97e8b1a9110efcf058dd3db7a169b305a
PBK-Bin <bin@PBK6.cn>
2021-03-06 20:40:01 +0800

    feat: Complete the home page layout

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,14 +16,41 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.LinkedList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,11 +63,169 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+@Composable
+fun SlidingTimeSelection(
+    modifier: Modifier?,
+    text: String = "",
+    minValue: Int = 0,
+    maxValue: Int = 60,
+    callBack: (value: Int) -> Unit? = {}
+) {
+
+    val TAG = "tzmax"
+    val initValue = 3
+
+    val listState = LazyListState()
+    var listIndex = remember {
+        mutableStateOf(minValue + initValue)
+    }
+    var lists = remember {
+        mutableStateOf(LinkedList<Int>((minValue..maxValue).toList()))
+    }
+
+    Row(
+        modifier = modifier!!,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+
+        LazyColumn(
+            state = listState,
+        ) {
+
+            items(
+                items = lists.value,
+                itemContent = { item ->
+
+                    DisposableEffect(listState) {
+                        onDispose {
+                            GlobalScope.launch {
+                                listState.run {
+                                    val info = this.layoutInfo
+                                    val i = info.visibleItemsInfo
+                                    listIndex.value = i[initValue].index
+                                    listState.animateScrollToItem(i[0].index)
+
+                                    callBack(listIndex.value)
+
+                                    Log.d(
+                                        TAG,
+                                        "SlidingTimeSelection() returned: $item ${listIndex.value} ${i[0].index}"
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    when (item) {
+                        listIndex.value -> {
+                            Text(
+                                text = if (item > 9) item.toString() else "0$item",
+                                textAlign = TextAlign.Center,
+                                color = Color(0xFF0000000),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 45.sp
+                            )
+                        }
+                        else -> {
+                            Text(
+                                text = if (item > 9) item.toString() else "0$item",
+                                textAlign = TextAlign.Center,
+                                color = Color(0x660000000),
+                                fontWeight = FontWeight.W300,
+                                fontSize = 45.sp
+                            )
+                        }
+                    }
+                }
+            )
+        }
+
+        Text(text = text, modifier = Modifier.padding(start = 5.dp))
+    }
+}
+
 // Start building your app here!
 @Composable
 fun MyApp() {
+
+    val _context = BaseApplication.getContext()
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+
+            Column(modifier = Modifier.padding(15.dp)) {
+                Text(text = _context.getString(R.string.app_name))
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                SlidingTimeSelection(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(335.dp),
+                    text = "Hour",
+                    maxValue = 23,
+                    callBack = fun(it: Int) {
+                        Log.d("tzmax", "选中的小时: $it")
+                    }
+                )
+
+                SlidingTimeSelection(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(335.dp),
+                    text = "Minute",
+                    callBack = fun(it: Int) {
+                        Log.d("tzmax", "选中的分钟: $it")
+                    }
+                )
+
+                SlidingTimeSelection(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(335.dp),
+                    text = "Second",
+                    callBack = fun(it: Int) {
+                        Log.d("tzmax", "选中的秒钟: $it")
+                    }
+                )
+            }
+
+            // 控制栏
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp, 20.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_action_play),
+                        contentDescription = "",
+                        Modifier
+                            .width(45.dp)
+                            .height(45.dp)
+                    )
+                }
+            }
+        }
     }
 }
 
